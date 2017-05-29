6910e44d6a97e5e875ab6f6c7fb266c65e0e4ace
Rativel <rativel@gmail.com>
2017-05-29 13:31:26 +0200

    Display rating value as fixed

--- client/src/Rating.js
+++ client/src/Rating.js
@@ -6,7 +6,7 @@ const Rating = ({value}) => (
     <p>
         <span style={{color: '#fa0'}}>{stars(~~value)}</span>
         <span style={{color: '#ddd'}}>{stars(5 - ~~value)}</span>
-        <span className="pl2 f5 black-80">{value}</span>
+        <span className="pl2 f5 black-80">{value && value.toFixed(1)}</span>
     </p>
 )
 
